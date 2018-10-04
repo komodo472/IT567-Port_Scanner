7ec527a57bdbff200634697a386be1577c9a5a21
komodo472 <logan.miller4721@gmail.com>
2018-10-04 17:46:46 -0600

    adding stub functions

--- scanner.py
+++ scanner.py
@@ -3,4 +3,18 @@ import argparse
 parser = argparse.ArgumentParser(description='Custom Port Scanner')
 parser.add_argument('hosts', help='hosts to be scanned')
 parser.add_argument('-p', '--ports', help='ports to be scanned. Defaults to common ports 0-1024')
+parser.add_argument('--tcp', help='scan tcp ports. Defaults to tcp')
+parser.add_argument('--udp', help='scan udp ports')
 parser.parse_args()
+
+def scan_tcp(hosts, ports):
+    return
+
+def scan_udp(hosts, ports):
+    return
+
+def parse_hosts(host_args):
+    return
+
+def parse_ports(port_args):
+    return
