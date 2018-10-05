848a9c8b848e64e96aad30a921d55c332b2946f7
komodo472 <logan.miller4721@gmail.com>
2018-10-05 10:28:59 -0600

    added ping sweep

--- scanner.py
+++ scanner.py
@@ -2,21 +2,24 @@ import argparse
 from scapy.all import *
 import re
 import ipaddress
+from collections import namedtuple
 
 parser = argparse.ArgumentParser(description='Custom Port Scanner')
 parser.add_argument('hosts', help='hosts to be scanned seperated by commas. Accepts single ips, ranges seperated by "-" and cidr')
 parser.add_argument('-p', '--ports', help='ports to be scanned. Defaults to common ports 1-1024')
-parser.add_argument('--tcp', help='scan tcp ports. Defaults to tcp')
+parser.add_argument('--tcp', help='scan tcp ports. Defaults to true')
 parser.add_argument('--udp', help='scan udp ports')
+parser.add_argument('--timeout', help='timeout in seconds for all functions. Default 5 sec')
+parser.add_argument('-v','--verbose', help='verbose output', action='store_true')
 args = parser.parse_args()
 
-def scan_tcp(hosts, ports):
+def scan_tcp(hosts, ports, args):
     return
 
-def scan_udp(hosts, ports):
+def scan_udp(hosts, ports, args):
     return
 
-def parse_hosts(host_args):
+def parse_hosts(host_args, args):
     host_list = host_args.split(',')
     hosts = list()
     for host in host_list:
@@ -28,7 +31,7 @@ def parse_hosts(host_args):
             hosts.append(host)
     return hosts
 
-def parse_ports(port_args):
+def parse_ports(port_args, args):
     if not port_args:
         return list(range(1, 1024))
     port_list = port_args.split(',')
@@ -42,8 +45,47 @@ def parse_ports(port_args):
             ports.append(port)
     return ports
 
-hosts = parse_hosts(args.hosts)
-ports = parse_ports(args.ports)
+def parse_extra_args(args):
+    extra_args = namedtuple('args', ['verbose', 'tcp', 'udp'])
+    extra_args.timeout = 5 if not args.timeout else int(args.timeout)
+    extra_args.verbose = True if args.verbose else False
+    extra_args.tcp = False if not args.tcp else True
+    extra_args.udp = True if args.udp else False
+    return extra_args
 
-print(hosts)
-print(ports)
+def ping_hosts(hosts, args):
+    hosts_alive = list()
+    hosts_down = list()
+    for host in hosts:
+        packet = IP(dst=host)/ICMP()
+        ans, unans = sr(packet, timeout=args.timeout, verbose=args.verbose)
+        for answered in ans:
+            hosts_alive.append(answered[1].src)
+        for unanswered in unans:
+            hosts_down.append(unanswered[0].dst)
+    return hosts_alive, hosts_down
+
+print("Starting scanner ...")
+extra_args = parse_extra_args(args)
+hosts = parse_hosts(args.hosts, extra_args)
+ports = parse_ports(args.ports, extra_args)
+print("Starting ping sweep ...")
+hosts_alive, hosts_down = ping_hosts(hosts, extra_args)
+print("Found %d alive hosts" % len(hosts_alive))
+
+if extra_args.verbose:
+    print("Alive hosts: %s" % hosts_alive)
+
+print("Found %d down hosts " % len(hosts_down))
+
+if extra_args.verbose:
+    print("Down hosts: %s" % hosts_down)
+
+print("Continuing with alive hosts ...")
+
+if extra_args.tcp:
+    print("Starting tcp scan ...")
+    scan_tcp(hosts_alive, ports, extra_args)
+if extra_args.udp:
+    print("Starting udp scan ...")
+    scan_udp(hosts_alive, ports, extra_args)
